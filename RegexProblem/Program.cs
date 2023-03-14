8957be6555a26ded7f2f7ec9f64b02366885cf35
rishiraj007 <rishiraj@mounttalent.com>
2023-03-14 16:02:01 +0530

    [Prabhu] Add: First name with Cap and 3 minimum characters

--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace RegexProblem;
+﻿using RegexProblem.UserRegistrationProblem;
+
+namespace RegexProblem;
 
 public class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("REGEX Practice Problem!");
+        UserRegistration userRegistration = new UserRegistration();
+        Console.WriteLine("WELCOME TO USER REGISTRATION");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("\nEnter option to choose \n1.First Name \n2.Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("\nEnter First Name");
+                    string fName = Console.ReadLine();
+                    userRegistration.validateFirstName(fName);
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
