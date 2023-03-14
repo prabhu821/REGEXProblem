8be4613607fd2778732259dce27ee9cf96103b41
rishiraj007 <rishiraj@mounttalent.com>
2023-03-14 16:22:52 +0530

    [Prabhu] Add: valid Email

--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -12,7 +12,7 @@ public class Program
         bool flag = true;
         while (flag)
         {
-            Console.WriteLine("\nEnter option to choose \n1.First Name \n2.Last Name \n3.Exit");
+            Console.WriteLine("\nEnter option to choose \n1.First Name \n2.Last Name \n3.Email \n4.Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -25,6 +25,11 @@ public class Program
                     Console.WriteLine("\nEnter Last Name");
                     string lName = Console.ReadLine();
                     userRegistration.validateLastName(lName);
+                    break;
+                case 3:
+                    Console.WriteLine("\nEnter Email");
+                    string email = Console.ReadLine();
+                    userRegistration.validateEmail(email);
                     break;
                 default:
                     flag = false;
--- RegexProblem/UserRegistrationProblem/UserRegistration.cs
+++ RegexProblem/UserRegistrationProblem/UserRegistration.cs
@@ -11,6 +11,7 @@ public class UserRegistration
 {
     public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
     public static string LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+    public static string Email = "^abc+.+[a-zA-Z0-9]+@+bl.co+.+[in|com]$";
 
     //UC1-FIRST NAME
     public void validateFirstName(string fName)
@@ -35,6 +36,19 @@ public class UserRegistration
         else
         {
             Console.WriteLine("First alphabet should be capital and Minimum 3 character");
+        }
+    }
+
+    //UC3-EMAIL
+    public void validateEmail(string email)
+    {
+        if (Regex.IsMatch(email, Email))
+        {
+            Console.WriteLine("Email Address : " + email);
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid email address ex :- abc.xyz@bl.co.in/.com");
         }
     }
 }
